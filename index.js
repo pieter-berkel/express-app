9fcaa6afa21a7c538738085a5075ab3dcd7e136b
Pieter Berkel <pieterberkel@msn.com>
2023-07-15 15:26:01 +0200

    feat: timeout test

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/timeout", async (req, res) => {
+  // simulate a delay and console log every 1 second
+  for (let i = 0; i < 120; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("On second: " + i);
+  }
+
+  res.send("Timeout test completed");
 });
 
 app.listen(port, () => {
